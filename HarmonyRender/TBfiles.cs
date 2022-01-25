43f52206db6f3b5db3a7d522c197a43f6e4f20c8
Denis Alenti <denis.alenti@gmail.com>
2022-01-25 14:36:08 +0100

    Add files via upload

---
+++ HarmonyRender/TBfiles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonyRender
+{
+    public class TBfiles
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string[] CharactersList { get; set; }
+        public int Frames { get; set; }
+        public string ExportName { get; set; }
+        public string ExportPath { get; set; }
+
+
+    }
+}
